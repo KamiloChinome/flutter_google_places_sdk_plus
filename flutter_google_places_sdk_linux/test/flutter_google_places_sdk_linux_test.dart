f28fb8e719bfa3baa60055d6639e5f26b3103da1
KamiloChinome <kamiloandresch@gmail.com>
2026-02-22 17:30:35 -0500

    test(desktop): add registration tests for windows, linux, and macos
    Add test suites for all three desktop packages verifying registerWith()
    sets the platform instance, class hierarchy (extends HttpPlugin and
    Platform), and platform instance assignment. Create test directories
    for linux and macos. Add flutter_test and dependency_overrides to all
    three pubspec files.

--- flutter_google_places_sdk_linux/test/flutter_google_places_sdk_linux_test.dart
+++ flutter_google_places_sdk_linux/test/flutter_google_places_sdk_linux_test.dart
@@ -1,30 +1,30 @@
 import 'package:flutter_google_places_sdk_http/flutter_google_places_sdk_http.dart';
+import 'package:flutter_google_places_sdk_linux/flutter_google_places_sdk_linux.dart';
 import 'package:flutter_google_places_sdk_platform_interface/flutter_google_places_sdk_platform_interface.dart';
-import 'package:flutter_google_places_sdk_windows/flutter_google_places_sdk_windows.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group('FlutterGooglePlacesSdkWindowsPlugin', () {
+  group('FlutterGooglePlacesSdkLinuxPlugin', () {
     test('registerWith sets platform instance', () {
-      FlutterGooglePlacesSdkWindowsPlugin.registerWith();
+      FlutterGooglePlacesSdkLinuxPlugin.registerWith();
       expect(
         FlutterGooglePlacesSdkPlatform.instance,
-        isA<FlutterGooglePlacesSdkWindowsPlugin>(),
+        isA<FlutterGooglePlacesSdkLinuxPlugin>(),
       );
     });
 
     test('extends FlutterGooglePlacesSdkHttpPlugin', () {
-      final plugin = FlutterGooglePlacesSdkWindowsPlugin();
+      final plugin = FlutterGooglePlacesSdkLinuxPlugin();
       expect(plugin, isA<FlutterGooglePlacesSdkHttpPlugin>());
     });
 
     test('extends FlutterGooglePlacesSdkPlatform', () {
-      final plugin = FlutterGooglePlacesSdkWindowsPlugin();
+      final plugin = FlutterGooglePlacesSdkLinuxPlugin();
       expect(plugin, isA<FlutterGooglePlacesSdkPlatform>());
     });
 
     test('can be used as platform instance', () {
-      final plugin = FlutterGooglePlacesSdkWindowsPlugin();
+      final plugin = FlutterGooglePlacesSdkLinuxPlugin();
       FlutterGooglePlacesSdkPlatform.instance = plugin;
       expect(FlutterGooglePlacesSdkPlatform.instance, same(plugin));
     });
--- flutter_google_places_sdk_macos/test/flutter_google_places_sdk_macos_test.dart
+++ flutter_google_places_sdk_macos/test/flutter_google_places_sdk_macos_test.dart
@@ -1,30 +1,30 @@
 import 'package:flutter_google_places_sdk_http/flutter_google_places_sdk_http.dart';
+import 'package:flutter_google_places_sdk_macos/flutter_google_places_sdk_macos.dart';
 import 'package:flutter_google_places_sdk_platform_interface/flutter_google_places_sdk_platform_interface.dart';
-import 'package:flutter_google_places_sdk_windows/flutter_google_places_sdk_windows.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group('FlutterGooglePlacesSdkWindowsPlugin', () {
+  group('FlutterGooglePlacesSdkMacosPlugin', () {
     test('registerWith sets platform instance', () {
-      FlutterGooglePlacesSdkWindowsPlugin.registerWith();
+      FlutterGooglePlacesSdkMacosPlugin.registerWith();
       expect(
         FlutterGooglePlacesSdkPlatform.instance,
-        isA<FlutterGooglePlacesSdkWindowsPlugin>(),
+        isA<FlutterGooglePlacesSdkMacosPlugin>(),
       );
     });
 
     test('extends FlutterGooglePlacesSdkHttpPlugin', () {
-      final plugin = FlutterGooglePlacesSdkWindowsPlugin();
+      final plugin = FlutterGooglePlacesSdkMacosPlugin();
       expect(plugin, isA<FlutterGooglePlacesSdkHttpPlugin>());
     });
 
     test('extends FlutterGooglePlacesSdkPlatform', () {
-      final plugin = FlutterGooglePlacesSdkWindowsPlugin();
+      final plugin = FlutterGooglePlacesSdkMacosPlugin();
       expect(plugin, isA<FlutterGooglePlacesSdkPlatform>());
     });
 
     test('can be used as platform instance', () {
-      final plugin = FlutterGooglePlacesSdkWindowsPlugin();
+      final plugin = FlutterGooglePlacesSdkMacosPlugin();
       FlutterGooglePlacesSdkPlatform.instance = plugin;
       expect(FlutterGooglePlacesSdkPlatform.instance, same(plugin));
     });
